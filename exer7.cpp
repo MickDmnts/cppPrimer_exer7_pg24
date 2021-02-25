9f4836757a7f2e449e263508d358d90fbf85949a
Michael Evangelos Diamantis <michaeldiamantis17@outlook.com>
2021-02-25 16:52:56 +0200

    All exercises completed.

--- exer7.cpp
+++ exer7.cpp
@@ -3,12 +3,33 @@
 
 void CountTransactions()
 {
+	int itemCount = 0;
+	Sales_item currentItem, item;
 
+	if (std::cin>>currentItem)
+	{
+		itemCount = 1;
+		while (std::cin>>item)
+		{
+			if (item.isbn()==currentItem.isbn())
+			{
+				++itemCount;
+			}
+			else
+			{
+				std::cout << currentItem.isbn() << " occurs " << itemCount << " times." << std::endl;
+				currentItem = item;
+				itemCount = 1;
+			}
+		}
+
+		std::cout << currentItem.isbn() << " occurs " << itemCount << " times." << std::endl;
+	}
 }
 
 int main()
 {
-	CountTransactions();
+	CountTransactions(); //Task 1.23 && 1.24
 	system("pause");
 
 	return 0;
